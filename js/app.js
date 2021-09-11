f2175e6099bcdaff7d08d66acc0e50062b93c65c
Vladyslav Tishchenko <fsknnn@gmail.com>
2021-09-11 05:20:23 +0300

    Create array constructor function.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 "use strict";
+
+function create2dArray(rows, cols) {
+  const outputArray = new Array(rows, cols);
+  let value = 1;
+
+  for (let row = 0; row < rows; row++) {
+    outputArray[row] = [];
+
+    for (let col = 0; col < cols; col++) {
+      outputArray[row][col] = value++;
+    }
+  }
+  return outputArray;
+}
+
+console.log(create2dArray(6, 5));
