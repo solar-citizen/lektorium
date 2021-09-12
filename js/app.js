e319f8a110f0e42b4a086358fc3ec4cd9029dadc
Vladyslav Tishchenko <fsknnn@gmail.com>
2021-09-13 01:28:05 +0300

    Create iterator function
    * It /just/ works. But algorythm is kinda wrong.
    * It'll take some time to find a right one.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function create2dArray(rows = 6, cols = 5) {
+function create2dArray(rows, cols) {
   const outputArray = new Array(rows, cols);
   let value = 1;
 
@@ -14,9 +14,55 @@ function create2dArray(rows = 6, cols = 5) {
   return outputArray;
 }
 
-function iterateArray(rows, cols, startRow, startCol, direction) {
-  const matrix = create2dArray(rows, cols);
+function iterateArray(
+  rows = 6,
+  cols = 5,
+  startRow = 4,
+  startCol = 2,
+  direction = "counter clockwise"
+) {
+  const matrix = create2dArray(rows, cols),
+    iteratedArray = [];
+  let startRowFix = startRow - 1,
+    startColFix = startCol - 1;
+
   console.log(matrix);
+
+  if (direction === "counter clockwise") {
+    while (startRowFix < rows && startColFix < cols) {
+      let value;
+      let i;
+
+      for (i = startColFix; i < cols; i++) {
+        value = matrix[startRowFix][i];
+        iteratedArray.push(value);
+      }
+      startRowFix++;
+
+      for (i = startRowFix; i < rows; i++) {
+        value = matrix[i][cols - 1];
+        iteratedArray.push(value);
+      }
+      cols--;
+
+      if (startRowFix < rows) {
+        for (i = cols - 1; i >= startColFix; i--) {
+          value = matrix[rows - 1][i];
+          iteratedArray.push(value);
+        }
+        rows--;
+      }
+
+      if (startColFix < cols) {
+        for (i = rows - 1; i >= startRowFix; i--) {
+          value = matrix[i][startColFix];
+          iteratedArray.push(value);
+        }
+        startColFix++;
+      }
+    }
+    console.log(iteratedArray);
+  }
 }
 
 iterateArray();
